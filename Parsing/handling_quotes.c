90cb6443cef74e82c53331b955533ca9d50c397c
Toufa7 <otoufah@student.1337.ma>
2022-07-22 21:40:20 +0100

    Fixing spaces inside double quotes

--- Parsing/handling_quotes.c
+++ Parsing/handling_quotes.c
@@ -1,63 +1,48 @@
 #include "../minishell.h"
 
-// char    *handling_quotes(char *str)
-// {
-//     int i = -1;
-//     char *dup = malloc(sizeof(char) * ft_strlen(str) + 1);
-//     if (!dup)
-//         return (NULL);
-//     while (str[++i])
-//     {
-//         if (str[i] == ' ')
-//             dup[i] = '%';
-//         else
-//             dup[i] = str[i];
-//     }
-//     dup[i] = '\0';
-//     return (dup);
-// }
-
-
-void    just_replace(char *str, char *dup, char alpha)
-{
-    int i = -1;
-    while (str[++i] != '"')
-    {
-        if (str[i] == alpha)
-            dup[i] = '~';
-        else
-            dup[i] = str[i];
-    }
-}
-
 char	*replace_spaces_with_x(char *str)
 {
-	int i = -1;
-	char *dup = malloc(sizeof(char) * ft_strlen(str + 1));
+	int i;
+    int j;
+    char *dup;
+
+    dup = malloc(sizeof(char) * (ft_strlen(str) + 1));
     if (!dup)
         return (NULL);
+    i = -1;
 	while (str[++i])
 	{
 		if (str[i] == '"')
-		{
-            int j = i;
+        {
+            j = i;
+            if (str[j] == '"')
+                dup[j] = str[j];
             while (str[++j] != '"')
             {
-                just_replace(str, dup, ' ');
+                if (str[j] == ' ')
+                {
+                    printf("Copy at %d\n",j);
+                    dup[j] = '~';
+                }
+                else
+                    dup[j] = str[j];
             }
-		}
-		else
-			dup[i] =  str[i];
-	}
-	dup[i] = '\0';
-	return (dup);
-
-}
-
-int main()
-{
-    while (1)
-    {
-        printf("%s\n",replace_spaces_with_x(readline("")));
+            if (str[j] == '"')
+                dup[j] = str[j];
+            i = j;
+        }
+        else
+            dup[i] = str[i];
     }
+	dup[i] = '\0';
+    return (dup);
 }
+
+
+// int main()
+// {
+//     while (1)
+//     {
+//         printf("%s\n",replace_spaces_with_x(readline("")));
+//     }
+// }
