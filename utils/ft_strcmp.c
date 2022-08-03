e6e80bdb67bf06796b7bf11d96cacf02be25d681
PØM$ <90482193+Toufa7@users.noreply.github.com>
2022-08-03 16:33:09 +0100

    Add files via upload

--- utils/ft_strcmp.c
+++ utils/ft_strcmp.c
@@ -16,8 +16,12 @@ int	ft_strcmp(const char *s1, const char *s2)
 {
 	int	i;
 
+	if (!s1 || !s2)
+		return (-1);
+	if (!s1 && !s2)
+		return (0);
 	i = 0;
-	while (s1[i] || s2[i])
+	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
--- utils/ft_strlen.c
+++ utils/ft_strlen.c
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
+	if (!s)
+		return (0);
 	i = 0;
 	while (s[i] != '\0')
 		i++;
